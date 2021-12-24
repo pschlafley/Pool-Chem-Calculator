186dd5bcbb545a95378e9ab97dcddcd459a31eae
Tom Chestnut <tchestnut85@gmail.com>
2021-12-24 11:02:31 -0500

    Refactored calculatePoolSalt variables and to return 0 if currentSalt >= the target PPM; added unit test for calculatePoolSalt

--- client/src/calculations/pool-salt.js
+++ client/src/calculations/pool-salt.js
@@ -1,24 +1,23 @@
-const calculatePoolSalt = (poolGallons, currentSalt) => {
+const calculatePoolSalt = (poolGallons = 10000, currentSalt = 3200) => {
 	// 1 lb of salt to 1,000 gallons of water to increase salt residual 120 ppm.
 
-	// variable for the amount of salt to add per 1000 gallons of water
-	let poundsPer10000 = 8;
+	const TARGET_PPM = 3200;
+	let poundsPer10000 = 8; // variable for the amount of salt to add per 1000 gallons of water
+	let lbsDifference;
 
 	if (poolGallons > 10000) {
-		let lbsDifference = (poolGallons - 10000) / 1000;
+		lbsDifference = (poolGallons - 10000) / 1000;
 		poundsPer10000 += lbsDifference;
 	} else if (poolGallons < 10000) {
-		let lbsDifference = (10000 - poolGallons) / 1000;
+		lbsDifference = (10000 - poolGallons) / 1000;
 		poundsPer10000 -= lbsDifference;
 	}
 
-	const AMOUNT_OF_PPM_NEEDED = (3200 - currentSalt) / 100;
+	const amountPpmNeeded = (3200 - currentSalt) / 100;
 
-	const SALT_TO_ADD = poundsPer10000 * AMOUNT_OF_PPM_NEEDED;
+	const saltToAdd = poundsPer10000 * amountPpmNeeded;
 
-	return SALT_TO_ADD;
+	return currentSalt >= TARGET_PPM ? 0 : saltToAdd;
 };
-
-calculatePoolSalt(6000, 2600);
 
 export default calculatePoolSalt;
