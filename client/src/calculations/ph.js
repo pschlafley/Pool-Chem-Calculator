62acc801397a369bdd21cf4e9b73f3c11ccaf869
Peyton Schlafley <pschlafley0@gmail.com>
2022-01-16 14:28:56 -0500

    started to create ph calculations

---
+++ client/src/calculations/ph.js
@@ -0,0 +1,9 @@
+const calculatePH = (gallons, currentPH) => {
+    // Ideal PH levels will be 7.2-7.8
+    // approximately 17 oz of muriatic acid will lower PH by .6
+    const M_A = 17;
+
+
+};
+
+calculatePH(10000, 8.0);
