4b234cdb27abc4dd934a3e329beca5f2a75c3741
Tom Chestnut <tchestnut85@gmail.com>
2022-01-17 11:21:06 -0500

    Return 0 from calculation if result is <= 0; updated form result displayed

--- client/src/calculations/pool-alkalinity.js
+++ client/src/calculations/pool-alkalinity.js
@@ -3,27 +3,27 @@
 // total alkalinity levels should be between 100 - 150 ppm (parts per million)
 
 export const calculateTotalAlkalinity = (poolGallons, alkalinityInPool) => {
-	// determine how much pounds of baking soda to use per gallon
-	const baseMultiplier = 1.5;
+  // determine how much pounds of baking soda to use per gallon
+  const baseMultiplier = 1.5;
 
-	// divide the total amount of poolGallons by 10000 to get it down to a base of 10
-	const gallonsBaseTen = poolGallons / 10000;
-	// console.log(`gal base ten: ${gallonsBaseTen}`)
+  // divide the total amount of poolGallons by 10000 to get it down to a base of 10
+  const gallonsBaseTen = poolGallons / 10000;
+  // console.log(`gal base ten: ${gallonsBaseTen}`)
 
-	// multiply gallonsBaseTen by 1.5 to get the amount of baking soda to raise the total alkalinity by 10 ppm (parts per million)
-	const bakingSodaToAdd = gallonsBaseTen * baseMultiplier;
-	// console.log(`bakingSodaToAdd: ${bakingSodaToAdd}`)
-	// subtract alkalinityInPool from 120 ppm to see how much we want to raise the total alkalinity in the pool to get it to 120 pmm
-	// In this case it will be 120 - 70 = 50...
-	// So that means we will need to raise the alkalinity levels by 50 ppm to get it to a good level
-	// I am then dividing it by 10 so that I can use the result of it to as a multiplier to the bakingSodaToAdd constant above
-	const alk = (120 - alkalinityInPool) / 10;
-	// console.log(`alk: ${alk}`)
-	// calculate the total amount of pounds of baking soda to add to the pool in order to raise it by a certain amount of parts per million
-	// in this case it will be 22.5 lbs of baking soda.
-	const totalPoundsToAdd = bakingSodaToAdd * alk;
-	// console.log(totalPoundsToAdd)
-	return totalPoundsToAdd.toFixed(1);
+  // multiply gallonsBaseTen by 1.5 to get the amount of baking soda to raise the total alkalinity by 10 ppm (parts per million)
+  const bakingSodaToAdd = gallonsBaseTen * baseMultiplier;
+  // console.log(`bakingSodaToAdd: ${bakingSodaToAdd}`)
+  // subtract alkalinityInPool from 120 ppm to see how much we want to raise the total alkalinity in the pool to get it to 120 pmm
+  // In this case it will be 120 - 70 = 50...
+  // So that means we will need to raise the alkalinity levels by 50 ppm to get it to a good level
+  // I am then dividing it by 10 so that I can use the result of it to as a multiplier to the bakingSodaToAdd constant above
+  const alk = (120 - alkalinityInPool) / 10;
+  // console.log(`alk: ${alk}`)
+  // calculate the total amount of pounds of baking soda to add to the pool in order to raise it by a certain amount of parts per million
+  // in this case it will be 22.5 lbs of baking soda.
+  const totalPoundsToAdd = bakingSodaToAdd * alk;
+  // console.log(totalPoundsToAdd)
+  return totalPoundsToAdd <= 0 ? 0 : totalPoundsToAdd.toFixed(1);
 };
 
 calculateTotalAlkalinity(30000, 70);
--- client/src/components/CalculationForm/AlkalinityForm/AlkalinityForm.js
+++ client/src/components/CalculationForm/AlkalinityForm/AlkalinityForm.js
@@ -6,73 +6,76 @@ import { calculateTotalAlkalinity } from '../../../calculations/pool-alkalinity'
 import styles from './AlkalinityForm.module.css';
 
 const AlkalinityForm = () => {
-	const [alkNeeded, setAlkNeeded] = useState(null);
+  const [alkNeeded, setAlkNeeded] = useState(null);
 
-	const initialValues = {
-		gallons: 10_000,
-		alkalinity: 100, // measured in PPM
-	};
+  const initialValues = {
+    gallons: 0,
+    alkalinity: 0, // measured in PPM
+  };
 
-	const schema = yup.object().shape({
-		gallons: yup.number().required(),
-		alkalinity: yup.number().required(),
-	});
+  const schema = yup.object().shape({
+    gallons: yup.number().required(),
+    alkalinity: yup.number().required(),
+  });
 
-	const handleCalculateAlk = ({ gallons, alkalinity }) => {
-		const poundsAlkNeeded = calculateTotalAlkalinity(gallons, alkalinity);
-		setAlkNeeded(poundsAlkNeeded);
-	};
+  const handleCalculateAlk = ({ gallons, alkalinity }) => {
+    const poundsAlkNeeded = calculateTotalAlkalinity(gallons, alkalinity);
+    console.log('poundsAlkNeeded:', poundsAlkNeeded);
+    const formattedResult =
+      poundsAlkNeeded <= 0 ? 'None' : Math.abs(poundsAlkNeeded);
+    setAlkNeeded(formattedResult);
+  };
 
-	return (
-		<Formik
-			initialValues={initialValues}
-			onSubmit={handleCalculateAlk}
-			validationSchema={schema}
-		>
-			{({ handleChange, handleSubmit, dirty, isValid }) => (
-				<form className={styles.form} onSubmit={handleSubmit}>
-					<h2>Calculate Alkalinity Needed</h2>
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleCalculateAlk}
+      validationSchema={schema}
+    >
+      {({ handleChange, handleSubmit, dirty, isValid }) => (
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <h2>Calculate Alkalinity Needed</h2>
 
-					{alkNeeded && (
-						<div className={styles.resultContainer}>
-							<p>Total Baking Soda to Add:</p>
-							<p className={styles.result}>{alkNeeded}</p>
-						</div>
-					)}
+          {alkNeeded && (
+            <div className={styles.resultContainer}>
+              <p>Total Baking Soda to Add:</p>
+              <p className={styles.result}>{alkNeeded}</p>
+            </div>
+          )}
 
-					<div className={styles.inputs}>
-						<div>
-							<label htmlFor='gallons'>Pool Gallons: </label>
-							<input
-								type='number'
-								name='gallons'
-								placeholder='Enter Gallons'
-								onChange={handleChange}
-							/>
-						</div>
+          <div className={styles.inputs}>
+            <div>
+              <label htmlFor='gallons'>Pool Gallons: </label>
+              <input
+                type='number'
+                name='gallons'
+                placeholder='Enter Gallons'
+                onChange={handleChange}
+              />
+            </div>
 
-						<div>
-							<label htmlFor='alkalinity'>Current Alkalinity (in PPM): </label>
-							<input
-								type='number'
-								name='alkalinity'
-								onChange={handleChange}
-								placeholder='Enter Alkalinity'
-							/>
-						</div>
+            <div>
+              <label htmlFor='alkalinity'>Current Alkalinity (in PPM): </label>
+              <input
+                type='number'
+                name='alkalinity'
+                onChange={handleChange}
+                placeholder='Enter Alkalinity'
+              />
+            </div>
 
-						<button
-							type='submit'
-							disabled={!dirty || !isValid}
-							className={styles.button}
-						>
-							Calculate
-						</button>
-					</div>
-				</form>
-			)}
-		</Formik>
-	);
+            <button
+              type='submit'
+              disabled={!dirty || !isValid}
+              className={styles.button}
+            >
+              Calculate
+            </button>
+          </div>
+        </form>
+      )}
+    </Formik>
+  );
 };
 
 export default AlkalinityForm;
