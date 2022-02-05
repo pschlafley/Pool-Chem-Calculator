efac13634b251310225e9bb6c648ab56dc772117
Peyton Schlafley <pschlafley0@gmail.com>
2022-02-05 11:20:45 -0500

    adding different styles for when a link in the navbar is focused

--- client/src/components/HomePage/home-page.js
+++ client/src/components/HomePage/home-page.js
@@ -3,10 +3,7 @@ import React from "react";
 function Home() {
     return (
         <>
-            <main>
-                <h2>Welcome to the homepage!</h2>
-                <p>You can do this, I believe in you.</p>
-            </main>
+
         </>
     );
 }
