c309009391c1dc0112cd781bf4c82053198e3ea2
Peyton Schlafley <pschlafley0@gmail.com>
2022-02-05 16:49:39 -0500

    added a comment on the home-page.js file, its to credit the author of the photo

--- client/src/components/HomePage/home-page.js
+++ client/src/components/HomePage/home-page.js
@@ -2,6 +2,10 @@ import React from "react";
 import styles from './hp.module.css';
 import DropDown from "../Dropdown/dropdown";
 
+// background image photo credits
+// Photo by <a href="https://unsplash.com/@josecasado?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">José Casado</a> on <a href="https://unsplash.com/s/photos/jumping-into-pool?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+
 function Home() {
     return (
         <>
