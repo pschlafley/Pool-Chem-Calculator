5b02f9161327c79dde78b99bd7e86e7ea030f1c1
Peyton Schlafley <pschlafley0@gmail.com>
2022-02-10 21:02:33 -0500

    changed background picture, and added media queries to make the homepage responsive. I still need to fix the media query for max-width: 2560px;

--- client/src/components/HomePage/home-page.js
+++ client/src/components/HomePage/home-page.js
@@ -4,17 +4,22 @@ import DropDown from "../Dropdown/dropdown";
 
 // background image photo credits
 // Photo by <a href="https://unsplash.com/@josecasado?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">José Casado</a> on <a href="https://unsplash.com/s/photos/jumping-into-pool?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-
+// Photo by Pixabay: https://www.pexels.com/photo/blue-lounger-beside-swimming-pool-221457/
 
 function Home() {
     return (
         <>
             <div className={styles.container}>
-                <div className={styles.backgroundImage}></div>
-                <div className={styles.textCenter}>
-                    <p>Welcome to the best pool calculator on the web!</p>
-                    <br />
-                    <DropDown />
+                <div className={styles.backgroundImage}>
+                    <div className={styles.contentContainer}>
+                        <div className={styles.ddPosition}>
+                            <DropDown />
+                        </div>
+                        <div className={styles.textCenter}>
+                            <p>Welcome to the best pool calculator on the web!</p>
+                            <br />
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
