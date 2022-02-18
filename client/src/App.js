bf2c965ef06303c25cd83d58970e1fa9f984d66b
Peyton Schlafley <pschlafley0@gmail.com>
2022-02-17 20:01:51 -0500

    moved the BrowserRouter component to the index.js file

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,17 @@ import NavBar from './components/Nav/navbar';
 import AlkalinityForm from './components/CalculationForms/AlkalinityForm/AlkalinityForm';
 import SaltForm from './components/CalculationForms/SaltForm/SaltForm';
 import Home from './components/HomePage/home-page';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/salt-form' element={<SaltForm />} />
-          <Route path='/alkalinity-form' element={<AlkalinityForm />} />
-        </Routes>
-      </Router>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/salt-form' element={<SaltForm />} />
+        <Route path='/alkalinity-form' element={<AlkalinityForm />} />
+      </Routes>
     </div>
   );
 }
