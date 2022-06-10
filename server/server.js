f378d4b32d57176de7e9065bdd5bfda968856916
Peyton Schlafley <pschlafley0@gmail.com>
2022-06-09 21:04:05 -0400

    removed the __dirname in the server.js file

--- server/server.js
+++ server/server.js
@@ -28,11 +28,11 @@ async function startServer(typeDefs, resolvers) {
   app.use(express.json());
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static('../client/build'));
   }
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile('../client/build/index.html');
   });
 
   db.once('open', async () => {
